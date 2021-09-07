edf0124788879e592ab8044ba22e1dee57ff2d05
EyalLevi0808 <fceyal@gmail.com>
2021-09-07 20:32:32 +0300

    Change the difault home page and add shop option to layout

--- Merchandise_Sport-master/Startup.cs
+++ Merchandise_Sport-master/Startup.cs
@@ -67,7 +67,7 @@ namespace Merchandise_Sport_master
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Users}/{action=Register}/{id?}");
             });
         }
     }
